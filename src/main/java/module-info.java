a7681dfb16d260c322cc99db987e019490c1bb89
eizybam <eizybam@outlook.com>
2026-03-30 23:25:06 -0600

    Fix Java version to 21 and update module exports for runnable app

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.resources.library {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
     requires org.kordamp.bootstrapfx.core;
 
@@ -9,4 +10,8 @@ module com.resources.library {
 
     opens com.resources.library.controllers to javafx.fxml;
     exports com.resources.library.controllers;
+    
+    exports com.resources.library.models;
+    exports com.resources.library.repositories;
+    exports com.resources.library.services;
 }
